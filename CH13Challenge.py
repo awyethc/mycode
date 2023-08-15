36d974116f9aa3c201a31deefabeb3fb9855f544
awyethc <clarkandreww@gmail.com>
2023-08-15 01:56:23 +0000

    added Chapter 13 challenge.

---
+++ CH13Challenge.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# Collect user name
+username = input("What is your name?")
+
+# Collect day of the week
+wkday = input("What day of the week is it?")
+
+#Print out a greeting with the collected variables.
+print("Hello, ",username,"! Happy ",wkday,"!", sep="")
+
