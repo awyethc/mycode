09bf970c9ad21c47b62ee8d702170fbcd9ba7e57
awyethc <141859149+awyethc@users.noreply.github.com>
2023-09-06 02:09:39 +0000

    After adding weather and biome, TEST2 is new bench

--- THEPythonProject/DND_CAMP_TEST2.py
+++ THEPythonProject/DND_CAMP_TEST2.py
@@ -22,15 +22,18 @@ import time
 class Party:
     """Create a Party class that will hold attributes"""
     def __init__(self):
-        self.camp_supplies = 80 #Start with 80 camp supplies.
-        self.short_rests = 2 #Short rest counter
+        self.camp_supplies = 80  # Start with 80 camp supplies
+        self.short_rests = 2
+        self.weather = "" # Starting as an empty string to be filled later
+        self.biome = "" # Starting as an empty string to be filled later
 
     def take_long_rest(self):
-        """Take a short rest if the party has more than 40 camp supplies."""
+        """Take a long rest if the party has more than 40 camp supplies."""
         if self.camp_supplies >= 40:
-            self.short_rests = 2 # Reset the short rest counter
-            self.camp_supplies -= 40 # Consume 40 Camp Supplies
-            print("\nYou consume 40 Camp Supplies and take a Long Rest.\n")
+            self.short_rests = 2  # Reset short rest counter
+            self.camp_supplies -= 40  # Consume 40 camp supplies
+            self.report_environment()  # Report the environment after the long rest
+            print("\nYou consume 40 Camp Supplies and take a Long Rest.")
             print("\nAll short rests have been replenished.")
         else:
             print("\nYou don't have enough Camp Supplies for a Long Rest.")
@@ -45,26 +48,47 @@ class Party:
             print("\nYou've run out of Short Rests. Consider a Long Rest.")
 
     def forage_supplies(self):
-        """Try to forage for supplies"""
+        """Try to forage for supplies in the current environment"""
         roll = random.randint(1, 20)
         if roll >= 10:
-            supplies_found = random.randint(1, 10)
+            supplies_found = self.calculate_forage_amount()
             self.camp_supplies += supplies_found
             print(f"\nYou successfully foraged {supplies_found} Camp Supplies.")
         else:
             print("\nYou couldn't find any additional Camp Supplies while foraging.")
 
+    def calculate_forage_amount(self):
+        """Calculate the amount of supplies to forage based on the environment."""
+        # Modify this logic based on your specific weather and biome factors
+        # For this example, we'll use a simple random amount between 1 and 10.
+        return random.randint(1, 10)
+
+    def report_environment(self):
+        """Report the current environment (weather and biome)."""
+        self.weather = random.choice(["Sunny", "Cloudy", "Rainy", "Stormy", "Foggy", "Snowy"])
+        self.biome = random.choice(["Forest", "Plains", "Island", "Mountain", "Swamp"])
+
     def show_status(self):
-        """Let the user check the party resources"""
+        """Let the user check the party resources and environment."""
         print(f"\nCamp Supplies: {self.camp_supplies}")
         print(f"Short Rests remaining: {self.short_rests}")
+        print(f"Weather: {self.weather}")
+        print(f"Biome: {self.biome}")
 
 def main():
     """Main function, which calls the Party class"""
     party = Party()
 
+    party.report_environment() # The first time the report environment function is called
+
+    # Initial message to the user on first start up
+    print("Welcome to the Rest Mechanics Simulator!\n")
+    print(f"You are in a {party.biome}, where the weather is currently {party.weather}.\n")
+    print(f"You have {party.camp_supplies} Camp Supplies remaining.")
+    time.sleep(1)
+
     while True:
-        print("\nChoose an action:")
+        print("\nChoose your next course of action:")
         print("1. Take a Long Rest")
         print("2. Take a Short Rest")
         print("3. Forage for Supplies")
@@ -82,9 +106,9 @@ def main():
             party.forage_supplies()
         elif choice == '4':
             party.show_status()
-            time.sleep(1)
+            time.sleep(2)
         elif choice == '5':
-            print("\nThank you for playing, this sim will exit in 3 seconds.\n")
+            print("\nThank you for playing, this sim will exit in 3 seconds.")
             time.sleep(3)
             break
         else:
